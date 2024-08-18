4ef155e11f3872b80d0844507b03ca3de7c1e626
philip white <whitephilip10@yahoo.com>
2024-08-18 16:34:42 -0500

    saave

--- code.js
+++ code.js
@@ -2,6 +2,32 @@
 function updatetext(){
 
     //button input and change output//
-
+    function init(){
+        imgObj = document.getElementById('myImage');
+        imgObj.style.position= 'relative';
+        imgObj.style.left = '0px';
+        }
+        
+        function startStop(){
+        moveRight();
+        change();
+        }
+        
+        function moveRight(){
+        imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+        animate = setTimeout(moveRight,50); 
+        }
+        
+        function change(){
+        var elem = document.getElementById("startButton");
+        if (elem.value=="Stop") elem.value = "Start";
+        else elem.value = "Stop";
+        }
+        
+        function stop(){
+        clearTimeout(animate);
+        }
+        
+        window.onload =init;
     document.getElementById("heading").innerHTML = "javascript is awesome"
 }
